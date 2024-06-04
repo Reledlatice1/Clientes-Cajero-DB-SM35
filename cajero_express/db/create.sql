d006a152b367f645892d2b83dcfb30ecfa3727f1
NaiveCannonUT <riquicraft04@gmail.com>
2024-06-04 09:47:50 -0500

    Nueva estructura y cambios

---
+++ cajero_express/db/create.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE cajero;
+USE cajero;
+
+CREATE TABLE tb_clientes(
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(25),
+    ap_paterno VARCHAR(25),
+    ap_materno VARCHAR(25)
+);
+
+CREATE TABLE tb_tarjeta(
+    id_tarjeta INT PRIMARY KEY AUTO_INCREMENT,
+    n_tarjeta VARCHAR(16),
+    nip VARCHAR(4),
+    saldo DECIMAL(20, 2),
+    id_cliente INT NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES tb_clientes(id_cliente)
+);
+
+CREATE TABLE tb_tipo_movimiento(
+    id_tipo_movimiento INT PRIMARY KEY AUTO_INCREMENT,
+    id_tipo_movimiento VARCHAR(20)
+);
+
+CREATE TABLE movimientos(
+    id_movimiento INT PRIMARY KEY AUTO_INCREMENT,
+    monto DECIMAL(7,2 ),
+    fecha_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id_tarjeta INT,
+    id_tipo_movimiento INT,
+    FOREIGN KEY (id_tarjeta) REFERENCES tb_tarjeta(id_tarjeta),
+    FOREIGN KEY (id_tipo_movimiento) REFERENCES tb_tipo_movimiento(id_tipo_movimiento)
+);
+
+/*
+<---formularios--->
+DATE 1990-09-25 
+TIME 10:05:25
+DATETIME 1990-09-25 10:05:25 NOW() <- INSERTA LA FECHA Y HORA DEL SERVIDOR
+YEAR 1990
+
+<---datos automaticos--->
+TIMESTAMP CURRENT_TIMESTAMP: HORA Y FECHA ACTUAL DE ACUERDO A LA ZONA HORARIA DEL REGISTRO
+*/
